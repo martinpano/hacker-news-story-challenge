cef8ea095390ae410106d976a0724a966da786d3
mpanovski <panovski.martin93@gmail.com>
2023-08-12 15:25:26 +0200

    Story Controller
    1) Implemented StoryController to fetch all stories

---
+++ HackerNews.Stories/HackerNews.Stories.Web/Controllers/StoryController.cs
@@ -0,0 +1,31 @@
+﻿using HackerNews.Stories.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNews.Stories.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoryController : ControllerBase
+    {
+        private readonly IStoryService _storyService;
+        public StoryController(IStoryService storyService) 
+        {
+            _storyService = storyService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var stories = await _storyService.GetAllStories();
+                return Ok(stories);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
